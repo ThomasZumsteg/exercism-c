b21d09484e2e60d424ac00f0b913f0a4d40ba783
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-07-02 20:49:30 -0700

    Faster isogram

--- isogram/src/isogram.c
+++ isogram/src/isogram.c
@@ -1,25 +1,17 @@
 #include <ctype.h>
-#include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 #include "isogram.h"
 
-int cmp(const void *e1, const void *e2) {
-    return *(char*)e1 - *(char*)e2;
-}
-
 int isIsogram(char *word) {
-    char *letters = malloc(sizeof(char) * strlen(word));
-    for(int i = 0; i < (int)strlen(word); i++)
-        letters[i] = tolower(word[i]);
-    qsort(letters, strlen(letters), sizeof(char), cmp);
-    for(int i = 1; i < (int)strlen(word); i++) {
-        if( letters[i] < 'a' ) 
+    int word_length = strlen(word);
+    char letter;
+    for (int i = 0; i + 1 < word_length; i++) {
+        letter = tolower(word[i]);
+        if( letter < 'a' || 'z' < letter)
             continue;
-        else if( letters[i] > 'z' )
-            break;
-        else if( letters[i] == letters[i - 1])
-            return 0;
+        for (int j = i + 1; j < word_length; j++) 
+            if(tolower(word[j]) == letter)
+                return 0;
     }
     return 1;
 }
