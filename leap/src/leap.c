58f3856454d974aa6e44682974b12dff956d031b
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-07-03 11:40:05 -0700

    First iteration leap

---
+++ leap/src/leap.c
@@ -0,0 +1,6 @@
+#include "leap.h"
+
+int is_leap_year(int year) {
+    return (year % 4 == 0 && year % 100 != 0) || 
+            year % 400 == 0;
+}
---
+++ leap/src/leap.h
@@ -0,0 +1,6 @@
+#ifndef LEAP_H
+#define LEAP_H
+
+int is_leap_year(int year);
+
+#endif
