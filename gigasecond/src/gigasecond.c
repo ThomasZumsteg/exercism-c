4bd72d402eaae40c8502d2fcc574cf6b913180a3
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-07-04 20:59:42 -0700

    First iteration gigasecond

---
+++ gigasecond/src/gigasecond.c
@@ -0,0 +1,7 @@
+#include "gigasecond.h"
+
+time_t gigasecond_after(time_t begin) {
+    return begin + GIGASECOND;
+
+}
+
---
+++ gigasecond/src/gigasecond.h
@@ -0,0 +1,9 @@
+#ifndef GIGASECOND_H
+#define GIGASECOND_H
+
+#define GIGASECOND 1000000000
+#include <time.h>
+
+time_t gigasecond_after(time_t begin);
+
+#endif
