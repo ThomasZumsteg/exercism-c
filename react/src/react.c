23a3fb66c53b04d52e11a4460cb3ef37ce093214
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-08-07 21:18:16 -0700

    Working cells

--- react/src/react.c
+++ react/src/react.c
@@ -1,19 +1,29 @@
 #include "react.h"
 #include <stdlib.h>
 #include <stdio.h>
+#define DEBUG(x) x
 #define MAX_CALLBACKS 10
 
+struct dep {
+    struct cell *source;
+    struct cell *dest;
+    struct dep *next;
+};
+
 struct reactor {
-    void (*update)(struct reactor *);
-    void *func;
-    struct cell **inputs;
-    int n_inputs;
-    struct cell *output;
+    struct dep *first;
+    struct dep *last;
+    void *func[MAX_CALLBACKS];
+    int max_call_id;
 };
 
 struct cell {
     int value;
     struct reactor *reactor;
+    void (*update)(struct cell *);
+    struct cell **inputs;
+    void *func;
+    int n_inputs;
 };
 
 struct reactor *create_reactor() {
@@ -23,52 +33,88 @@ struct reactor *create_reactor() {
 struct cell *create_input_cell(struct reactor *r, int initial_value) {
     struct cell *c = calloc(sizeof(struct cell), 1);
     c->reactor = r;
+    DEBUG(printf("Created cell %p\n", (void *)c));
     set_cell_value(c, initial_value);
     return c;
 }
 
-void update_compute1(struct reactor *reactor) {
-    struct cell *output = reactor->output;
-    compute1 func = (int (*)(int))reactor->func;
-    int input = get_cell_value(reactor->inputs[0]);
-    set_cell_value(output, func(input));
+void update_compute1(struct cell *cell) {
+    DEBUG(printf("Updating compute1 cell: %p\n", (void *)cell));
+    compute1 func = (int (*)(int))cell->func;
+    int input = get_cell_value(cell->inputs[0]);
+    set_cell_value(cell, func(input));
+}
+
+void add_dependancy(struct reactor *r, struct cell *source, struct cell *dest) {
+    struct dep *d = calloc(sizeof(struct dep), 1);
+    DEBUG(printf("Adding dependancy %p -> %p\n", (void *)source, (void *)dest));
+    d->source = source;
+    d->dest = dest;
+    if(r->first == NULL) {
+        r->first = d;
+        r->last = d;
+    } else
+        r->last->next = d;
+    r->last = d;
 }
 
 struct cell *create_compute1_cell(struct reactor *r, struct cell *input,
         compute1 func) {
-    r->output = create_input_cell(r, func(input->value));
-    r->func = (void *)func;
-    r->inputs = &input;
-    r->update = &update_compute1;
-    return r->output;
+    struct cell *cell = calloc(sizeof(struct cell),1);
+    cell->func = (void *)func;
+    cell->reactor = r;
+
+    cell->inputs = calloc(sizeof(struct cell *), 1);
+    cell->inputs[0] = input;
+    add_dependancy(r, input, cell);
+
+    cell->update = &update_compute1;
+    cell->update(cell);
+
+    return cell;
 }
 
-void update_compute2(struct reactor *reactor) {
-    struct cell *output = reactor->output;
-    compute2 func = (int (*)(int, int))reactor->func;
-    int input1 = get_cell_value(reactor->inputs[0]);
-    int input2 = get_cell_value(reactor->inputs[1]);
-    set_cell_value(output, func(input1, input2));
+void update_compute2(struct cell *cell) {
+    DEBUG(printf("Updating compute1 cell: %p\n", (void *)cell));
+    compute2 func = (int (*)(int, int))cell->func;
+    int input1 = get_cell_value(cell->inputs[0]);
+    int input2 = get_cell_value(cell->inputs[1]);
+    set_cell_value(cell, func(input1, input2));
 }
 
 struct cell *create_compute2_cell(struct reactor *r, struct cell *input1,
         struct cell *input2, compute2 func) {
-    r->output = create_input_cell(r, func(input1->value, input2->value));
-    r->func = (void *)func;
-    r->inputs = calloc(sizeof(struct cell), 2);
-    r->inputs[0] = input1;
-    r->inputs[1] = input2;
-    return r->output;
+    struct cell *cell = calloc(sizeof(struct cell), 1);
+    cell->reactor = r;
+    cell->func = (void *)func;
+
+    cell->inputs = calloc(sizeof(struct cell *), 2);
+    cell->inputs[0] = input1;
+    cell->inputs[1] = input2;
+    add_dependancy(r, input1, cell);
+    add_dependancy(r, input2, cell);
+
+    cell->update = &update_compute2;
+    cell->update(cell);
+
+    return cell;
 }
 
 int get_cell_value(struct cell *c) {
+    DEBUG(printf("Getting cell value %p\n", (void *)c));
     return c->value;
 }
 
 void set_cell_value(struct cell *c, int new_value) {
+    DEBUG(printf("Setting cell value %p\n", (void *)c));
+    struct reactor *r = c->reactor;
     c->value = new_value;
-    if(c->reactor->update != NULL) {
-        c->reactor->update(c->reactor);
+    for(struct dep *d = r->first; d != NULL; d = d->next) {
+        if(d->source == c) {
+            DEBUG(printf("Updating depenancy %p -> %p\n", (void *)d->source,
+                    (void *)d->source));
+            d->dest->update(d->dest);
+        }
     }
 }
 
