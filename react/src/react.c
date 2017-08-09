5893b87e7488e87ff084098991ed7db589fdae77
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-08-08 21:24:04 -0700

    Work on react

--- react/src/react.c
+++ react/src/react.c
@@ -4,16 +4,16 @@
 #define DEBUG(x) x
 #define MAX_CALLBACKS 10
 
-struct dep {
-    struct cell *source;
-    struct cell *dest;
-    struct dep *next;
+struct call {
+    int id;
+    void *args;
+    callback callback;
+    struct call *next;
 };
 
 struct reactor {
-    struct dep *first;
-    struct dep *last;
-    void *func[MAX_CALLBACKS];
+    struct call *first;
+    struct call *last;
     int max_call_id;
 };
 
@@ -38,24 +38,12 @@ struct cell *create_input_cell(struct reactor *r, int initial_value) {
     return c;
 }
 
-void update_compute1(struct cell *cell) {
-    DEBUG(printf("Updating compute1 cell: %p\n", (void *)cell));
+void compute1_callback(void *data, int id) {
+    DEBUG(printf("Updating compute1 cell: %d\n", id));
+    struct cell *cell = (struct cell *)data;
     compute1 func = (int (*)(int))cell->func;
     int input = get_cell_value(cell->inputs[0]);
     set_cell_value(cell, func(input));
-}
-
-void add_dependancy(struct reactor *r, struct cell *source, struct cell *dest) {
-    struct dep *d = calloc(sizeof(struct dep), 1);
-    DEBUG(printf("Adding dependancy %p -> %p\n", (void *)source, (void *)dest));
-    d->source = source;
-    d->dest = dest;
-    if(r->first == NULL) {
-        r->first = d;
-        r->last = d;
-    } else
-        r->last->next = d;
-    r->last = d;
 }
 
 struct cell *create_compute1_cell(struct reactor *r, struct cell *input,
@@ -66,16 +54,15 @@ struct cell *create_compute1_cell(struct reactor *r, struct cell *input,
 
     cell->inputs = calloc(sizeof(struct cell *), 1);
     cell->inputs[0] = input;
-    add_dependancy(r, input, cell);
 
-    cell->update = &update_compute1;
-    cell->update(cell);
+    add_callback(cell, (void *)cell, compute1_callback);
 
     return cell;
 }
 
-void update_compute2(struct cell *cell) {
-    DEBUG(printf("Updating compute1 cell: %p\n", (void *)cell));
+void compute2_callback(void *data, int id) {
+    DEBUG(printf("Updating compute1 cell: %d\n", id));
+    struct cell *cell = (struct cell *)data;
     compute2 func = (int (*)(int, int))cell->func;
     int input1 = get_cell_value(cell->inputs[0]);
     int input2 = get_cell_value(cell->inputs[1]);
@@ -91,12 +78,8 @@ struct cell *create_compute2_cell(struct reactor *r, struct cell *input1,
     cell->inputs = calloc(sizeof(struct cell *), 2);
     cell->inputs[0] = input1;
     cell->inputs[1] = input2;
-    add_dependancy(r, input1, cell);
-    add_dependancy(r, input2, cell);
 
-    cell->update = &update_compute2;
-    cell->update(cell);
-
+    add_callback(cell, (void *)cell, compute2_callback);
     return cell;
 }
 
@@ -109,11 +92,10 @@ void set_cell_value(struct cell *c, int new_value) {
     DEBUG(printf("Setting cell value %p\n", (void *)c));
     struct reactor *r = c->reactor;
     c->value = new_value;
-    for(struct dep *d = r->first; d != NULL; d = d->next) {
-        if(d->source == c) {
-            DEBUG(printf("Updating depenancy %p -> %p\n", (void *)d->source,
-                    (void *)d->source));
-            d->dest->update(d->dest);
+    for(struct call *d = r->first; d != NULL; d = d->next) {
+        if(d->id == (int)c) {
+            DEBUG(printf("Callback on %p\n", (void *)c));
+            d->callback(d->args, d->id);
         }
     }
 }
@@ -123,8 +105,18 @@ void destroy_reactor(struct reactor *r) {
 }
 
 callback_id add_callback(struct cell *c, void *args, callback func) {
-    if(c && args && func) {}
-    return 0;
+    struct call *call = malloc(sizeof(call));
+    call->id = (int)c;
+    call->callback = func;
+    call->args = args; 
+    if(c->reactor->first == NULL) {
+        c->reactor->first = call;
+        c->reactor->last = call;
+    } else {
+        c->reactor->last->next = call;
+        c->reactor->last = call;
+    }
+    return call->id;
 }
 
 void remove_callback(struct cell *c, callback_id id) {
