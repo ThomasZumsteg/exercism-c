599da525c4daa289987c274d066a1a3259442939
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-08-15 21:26:06 -0700

    First iteration react

--- react/src/react.c
+++ react/src/react.c
@@ -1,15 +1,21 @@
 #include "react.h"
 #include <stdlib.h>
 #include <stdio.h>
-#define DEBUG(x) x
+#define DEBUG(x)
+#define MAX_CELLS 10
 
 struct reactor {
-    struct cell *cells;
+    struct cell **cells;
+    int n_cells;
 };
 
 struct cell {
     int value;
-    struct cell *next;
+    void (*update)(struct cell *);
+    struct reactor *reactor;
+    void *func;
+    struct cell **inputs;
+    int n_inputs;
     callback_id ids;
     struct callback_data *callbacks;
 };
@@ -17,84 +23,94 @@ struct cell {
 struct reactor *create_reactor() {
     DEBUG(printf("Creating reactor\n"));
     struct reactor *r = malloc(sizeof(struct reactor));
-    r->cells = NULL;
+    r->cells = calloc(sizeof(struct cell *), MAX_CELLS); 
+    r->n_cells = 0;
     return r;
 }
 
-struct cell *create_cell(struct reactor *r, int value) {
+void update_reactor(struct reactor *r) {
+    DEBUG(printf("Updating reactor\n"));
+    for(int i = 0; i < r->n_cells; i++) {
+        DEBUG(printf("Next %p\n", (void *)r->cells[i]));
+        r->cells[i]->update(r->cells[i]);
+    }
+}
+
+struct cell *create_cell(struct reactor *r, void update(struct cell *),
+        void *func, int value) {
     struct cell *c = malloc(sizeof(struct cell));
-    c->next = r->cells;
-    r->cells = c;
+    r->cells[r->n_cells++] = c;
+    c->reactor = r;
     c->value = value;
+    c->update = update;
+    c->func = func;
     c->callbacks = NULL;
     c->ids = 0;
     return c;
 }
 
-struct cell *create_input_cell(struct reactor *r, int initial_value) {
-    DEBUG(printf("Creating input cell\n"));
-    return create_cell(r, initial_value);
+void noop(struct cell *input) {
+    DEBUG(printf("Input update\n"));
+    (void)input;
 }
 
-struct compute1_data { 
-    compute1 func;
-    struct cell *output;
+struct cell *create_input_cell(struct reactor *r, int initial_value) {
+    DEBUG(printf("Creating input cell\n"));
+    struct cell *c = create_cell(r, noop, NULL, initial_value);
+    return c;
+}
+
+struct callback_data {
+    struct callback_data *next;
+    callback func;
+    void *data;
+    int id;
 };
 
-void update_compute1(void *data, int value) {
+void run_callbacks(struct cell *c) {
+    for(struct callback_data *p = c->callbacks; p != NULL; p = p->next ) {
+        DEBUG(printf("Running callback %p: %d\n", (void *)c, p->id));
+        p->func(p->data, c->value);
+    }
+}
+
+void update_compute1(struct cell *c) {
     DEBUG(printf("Compute 1 update\n"));
-    struct compute1_data *cp1_data = (struct compute1_data *)data;
-    set_cell_value(cp1_data->output, cp1_data->func(value));
+    int new_value = ((compute1)c->func)(get_cell_value(c->inputs[0]));
+    if( new_value == c->value)
+        return;
+    c->value = new_value;
+    run_callbacks(c);
 }
 
 struct cell *create_compute1_cell(struct reactor *r, struct cell *input,
         compute1 func) {
     DEBUG(printf("Creating compute1 cell \n"));
-    struct compute1_data *data = malloc(sizeof(struct compute1_data));
-    data->func = func;
-    data->output = create_cell(r, func(get_cell_value(input)));
-    add_callback(input, data, update_compute1);
-    return data->output;;
+    struct cell *c = create_cell(r, update_compute1, (void *)func,
+            func(get_cell_value(input)));
+    c->inputs = calloc(sizeof(struct cell *), 1);
+    c->inputs[0] = input;
+    return c;
 }
 
-struct compute2_data {
-    compute2 func;
-    struct cell *input;
-    struct cell *output;
-    int left;
-};
-
-void update_compute2(void *data, int value) {
+void update_compute2(struct cell *c) {
     DEBUG(printf("Compute 2 update\n"));
-    struct compute2_data *d = (struct compute2_data *)data;
-    int right = get_cell_value(d->input);
-    int left  = value;
-    if(d->left != 0) {
-        left = right;
-        right = value;
-    }
-    set_cell_value(d->output, d->func(left, right)); 
+    int new_value = ((compute2)c->func)(get_cell_value(c->inputs[0]),
+            get_cell_value(c->inputs[1]));
+    if(new_value == c->value)
+        return;
+    c->value = new_value;
+    run_callbacks(c);
 }
 
 struct cell *create_compute2_cell(struct reactor *r, struct cell *input1,
         struct cell *input2, compute2 func) {
     DEBUG(printf("Creating compute2 cell\n"));
-    struct cell *c = create_cell(r, 
+    struct cell *c = create_cell(r, update_compute2, (void *)func, 
             func(get_cell_value(input1), get_cell_value(input2)));
-    struct compute2_data *left = malloc(sizeof(struct compute2_data));
-    left->func = func;
-    left->input = input2;
-    left->output = c;
-    left->left = 1;
-    add_callback(input1, left, update_compute2);
-
-    struct compute2_data *right = malloc(sizeof(struct compute2_data));
-    right->func = func;
-    right->input = input1;
-    right->output = c;
-    right->left = 0;
-    add_callback(input2, right, update_compute2);
-
+    c->inputs = calloc(sizeof(struct cell *), 2);
+    c->inputs[0] = input1;
+    c->inputs[1] = input2;
     return c;
 }
 
@@ -103,44 +119,17 @@ int get_cell_value(struct cell *c) {
     return c->value;
 }
 
-struct callback_data {
-    callback func;
-    void *data;
-    int id;
-    struct callback_data *next;
-};
-
-void destroy_callback_data(struct callback_data *d) {
-    DEBUG(printf("Destorying data %p\n", (void *)d));
-    free(d);
-}
-
 void set_cell_value(struct cell *c, int new_value) {
     DEBUG(printf("Setting cell value %p: %d\n", (void *)c, new_value));
     if(new_value == c->value)
         return;
     c->value = new_value;
-    for(struct callback_data *p = c->callbacks; p != NULL; p = p->next ) {
-        DEBUG(printf("Running callback %p: %d\n", (void *)c, p->id));
-        p->func(p->data, new_value);
-    }
-}
-
-void destroy_cell(struct cell *c) {
-    DEBUG(printf("Destorying cell %p\n", (void *)c));
-    for(struct callback_data *d = c->callbacks; d != NULL; d = c->callbacks) {
-        c->callbacks = d->next;
-        destroy_callback_data(d);
-    }
-    free(c);
+    run_callbacks(c);
+    update_reactor(c->reactor);
 }
 
 void destroy_reactor(struct reactor *r) {
     DEBUG(printf("Destorying reactor %p\n", (void *)r));
-    for(struct cell *c = r->cells; c != NULL; c = r->cells) {
-        r->cells = c->next;
-        destroy_cell(c); 
-    }
     free(r);
 }
 
@@ -166,7 +155,6 @@ void remove_callback(struct cell *c, callback_id id) {
                 c->callbacks = data->next;
             else
                 prev->next = data->next;
-            destroy_callback_data(data);
             return;
         }
     }
