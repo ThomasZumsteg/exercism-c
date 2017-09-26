1ef403f7ce5a05af62567d44d7a033f85241194f
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2017-09-25 18:02:46 -0700

    Using arrays for nth-prime

--- nth-prime/src/nth_prime.c
+++ nth-prime/src/nth_prime.c
@@ -6,39 +6,9 @@
 
 int n_primes;
 
-typedef struct node {
-    uint32_t val;
-    struct node* next;
-    struct node* prev;
-} node;
-
-void append(node **root, uint32_t value) {
-    node *n = malloc(sizeof(node));
-    n->val = value;
-    if(*root == NULL) {
-        n->next = n;
-        n->prev = n;
-        *root = n;
-        n_primes = 0;
-    } else {
-        n->prev = (*root)->prev;
-        n->next = (*root)->prev->next;
-        (*root)->prev->next = n;
-        (*root)->prev = n;
-    } 
-    n_primes++;
-}
-
-uint32_t get(node *root, int nth) {
-    node *n = root;
-    for(int i = 1; i < nth; i++)
-        n = n->next;
-    return n->val;
-}
-
-int prime(uint32_t num, node *primes) {
-    for(node *n = primes; n->val * n->val <= num; n = n->next){
-        if(num % n->val == 0)
+int prime(uint32_t num, uint32_t primes[]) {
+    for(uint32_t *n = primes; *n * *n <= num; n++ ) {
+        if(num % *n == 0)
             return 0;
     }
     return 1;
@@ -46,14 +16,15 @@ int prime(uint32_t num, node *primes) {
 
 uint32_t nth(int n) {
     if(n <= 0) return 0;
-    struct node *primes;
-    append(&primes, 2);
-    append(&primes, 3);
+    uint32_t *primes = calloc(n, sizeof(uint32_t));
+    n_primes = 2;
+    primes[0] = 2;
+    primes[1] = 3;
     for(uint32_t p = 5; n_primes < n; p+=2) {
         if(prime(p, primes))
-            append(&primes, p);
+            primes[n_primes++] = p;
 
     }
-    return get(primes, n);
+    return primes[n-1];
 }
 
