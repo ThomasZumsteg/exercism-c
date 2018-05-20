1df7a529ca8dc280359c6fc77e8bd3700b71e5ad
Thomas Zumsteg <Thomas.Zumsteg@gmail.com>
2018-05-19 17:23:34 -0700

    Second iteration scrabble_score

--- scrabble-score/src/scrabble_score.c
+++ scrabble-score/src/scrabble_score.c
@@ -3,19 +3,19 @@
 #include <string.h>
 
 int letter_val(char c) {
-    if(strchr("AEIOULNRST", c) != 0)
+    if(strchr("AEIOULNRST", c))
        return 1;
-   else if(strchr("DG", c) != 0)
+   else if(strchr("DG", c))
        return 2;
-    else if(strchr("BCMP", c) != 0)
+    else if(strchr("BCMP", c))
         return 3;
-    else if(strchr("FHVWY", c) != 0)
+    else if(strchr("FHVWY", c))
         return 4;
-    else if(strchr("K", c) != 0)
+    else if(strchr("K", c))
         return 5;
-    else if(strchr("JX", c) != 0)
+    else if(strchr("JX", c))
         return 8;
-    else if(strchr("QZ", c) != 0)
+    else if(strchr("QZ", c))
         return 10;
     return -1;
 }
